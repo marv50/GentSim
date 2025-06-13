51212e4b37823219dd8978a25be384d4b38d3e59
Luke-Kr <lukraakman@gmail.com>
2025-06-13 14:37:52 +0200

    Added gamma function

--- src/household.py
+++ src/household.py
@@ -7,6 +7,7 @@ class Household(Agent):
     A household agent in the GentSim model.
     """
     def __init__(self, model: Model) -> None:
+        self.model = model
         super().__init__(model)
         self.income = 0  # Pooruhh
 
@@ -16,3 +17,12 @@ class Household(Agent):
         """
         #I cant afford? -> MOVE
         pass
+
+def income_percentile(model, income, x, y) -> float:
+    """
+    Calculate the income percentile of the household.
+    """
+    assert income > 0,"Income must be greater than 0"
+    neighbours = model.grid.get_neighbours((x, y), True, False)
+    total = sum([n.income for n in neighbours])
+    return income / (total + income)
