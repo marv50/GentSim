d7798997e84a992f05221e138a6bff18e90b7f71
kingilsildor <tychostam@gmail.com>
2025-06-13 15:26:52 +0200

    add move in and out for medium income

--- src/household.py
+++ src/household.py
@@ -29,11 +29,30 @@ def income_percentile(model, income, x, y) -> float:
 
 
 def move_out_medium():
+    """
+    Calculate the probability of moving out based on the income percentile.
+    """
     p = 4 * (income_percentile() - 0.5)**2
     assert 0 <= p <= 1
     return p
 
-def move_in_medium():
-    houses = {
-        (x, y)
+def move_in(model, utility_func):
+    """
+    Calculate the utility of moving into a house.
+    The utility is calculated as the inverse of the utility function.
+    Where the utility function depends on the income level
+    """
+    house_utilities = {
+        (x, y): 1 - utility_func() for (x,y) in model.empty_houses
     }
+    total_sum = sum(house_utilities.values())
+    rho = max(
+        value / (total_sum - value) if (total_sum - value) != 0 else 0
+        for value in house_utilities.values()
+    )
+    assert 0 <= rho <= 1
+    return rho
+
+    
+
+
