16a89e58103f7304b017c136afb28dbc10656946
Luke-Kr <lukraakman@gmail.com>
2025-06-18 11:42:21 +0200

    Actually keep track of empty houses

--- src/model.py
+++ src/model.py
@@ -13,10 +13,12 @@ class GentSimModel(Model):
     def __init__(self, N: int, n: int, theta: float) -> None:
         super().__init__()
         self.grid = SingleGrid(N*n, N*n, False)
-        self.num_agents = N * n
+        # self.num_agents = N * n
+        self.N = N
+        self.n = n
         self.theta = theta
         self.neighbourhoods = np.array([[Neighbourhood(i, j) for i in range(N)] for j in range(N)], dtype=Neighbourhood)
-        self.empty_houses = [(0,0), (2,0)] # temp list of the empty houses
+        self.empty_houses = np.zeros((N*n, N*n), dtype=bool)
         self.init_population(N, n, 0.5)
 
     def init_population(self, N: int, n: int, p: float) -> None:
@@ -27,6 +29,8 @@ class GentSimModel(Model):
             for j in range(N*n):
                 if self.random.random() < p:
                     agent = self.new_agent((i, j), N)
+                    self.empty_houses[i, j] = False
+                
 
     def new_agent(self, pos, N) -> None:
         """
