731afcf3fc8fabe5415bf14cea18096e1ffbf137
kingilsildor <tychostam@gmail.com>
2025-06-23 14:08:56 +0200

    added income distribution to script file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-from src.income_distribution import load_distribution, plot_income_distribution
 from src.model import GentSimModel
 from src.simulation_runner import single_run
+from scripts.create_plots import plot_income_distribution
 
 if __name__ == "__main__":
-
     single_run(10, 10, 10, 0.5, 1, 0.5, 10)
-    
+
+    plot_income_distribution(
+        title="Income Distribution in the Netherlands 2022",
+        xlabel=r"Income $\times 1000$ (in Euros)",
+        ylabel="Frequency",
+    )
