212e979c8211336e7ff565b7c756b67c7638ab99
marv50 <marvinfrommer00@gmail.com>
2025-06-24 14:33:11 +0200

    changed analysis

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -15,20 +15,3 @@ def average_across_runs(simulation_results):
     return np.mean(simulation_results, axis=0)
 
 
-if __name__ == "__main__":
-    df = multiple_runs(
-        n_agents=300,
-        n_neighborhoods=5,
-        n_houses=5,
-        epsilon=5,
-        p_h=0.2,
-        b=0.5,
-        r_moore=1,
-        sensitivity_param=2,
-        steps=10,
-        runs=2,
-    )
-
-path = "data/combined_agent_data.csv"
-array = multiple_run_grid(path, value_column='income')
-print(array.shape)  # Should print (runs, time_steps, height, width)
