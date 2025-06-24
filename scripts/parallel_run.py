a18d7878ec6498b5e566099915ba2ad237908fde
kingilsildor <tychostam@gmail.com>
2025-06-24 11:51:47 +0200

    added the sensitivity param

--- scripts/parallel_run.py
+++ scripts/parallel_run.py
@@ -1,18 +1,15 @@
-from src.model import GentSimModel
 from mesa.batchrunner import batch_run
 
-if __name__ == "__main__":
+from src.model import GentSimModel
 
+if __name__ == "__main__":
     params = {
         "N_agents": 50,
         "N_neighbourhoods": 100,
         "N_houses": 100,
-        "theta": 0.5,
         "epsilon": 1,
-        "p_h": 0.5
+        "p_h": 0.5,
+        "sensitivity_param": 2,
     }
 
-    results = batch_run(
-        GentSimModel,
-        parameters=params, 
-        number_processes=1)
+    results = batch_run(GentSimModel, parameters=params, number_processes=1)
